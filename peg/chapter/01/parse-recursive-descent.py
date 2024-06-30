fa97a85e06a206f823dafc4c184f0218d4a099aa
coalg <algebroid0@gmail.com>
2024-07-01 01:00:52 +0900

    fix implementation

--- peg/chapter/01/parse-recursive-descent.py
+++ peg/chapter/01/parse-recursive-descent.py
@@ -1,14 +1,15 @@
 def calc(s):
-    pos = s.find("*")
-    if pos > 0:
-        left = s[:pos]
-        right = s[pos+1:]
-        return calc(left) * calc(right)
     pos = s.find("+")
     if pos > 0:
         left = s[:pos]
         right = s[pos+1:]
         return calc(left) + calc(right)
+
+    pos = s.find("*")
+    if pos > 0:
+        left = s[:pos]
+        right = s[pos+1:]
+        return calc(left) * calc(right)
     return float(s)
 
 assert calc("1+2+3") == 6.0
