3a4b351773603cad80ec7fba4e489a1a166db56b
coalg <algebroid0@gmail.com>
2024-07-01 01:00:01 +0900

    buggy implementation for multiply

--- peg/chapter/01/parse-recursive-descent.py
+++ peg/chapter/01/parse-recursive-descent.py
@@ -1,4 +1,9 @@
 def calc(s):
+    pos = s.find("*")
+    if pos > 0:
+        left = s[:pos]
+        right = s[pos+1:]
+        return calc(left) * calc(right)
     pos = s.find("+")
     if pos > 0:
         left = s[:pos]
@@ -7,3 +12,5 @@ def calc(s):
     return float(s)
 
 assert calc("1+2+3") == 6.0
+assert calc("1+2*3") == 7.0
+assert calc("1*2+3") == 5.0
