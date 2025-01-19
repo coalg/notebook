070c4770e459c332836279f687c1c755e3339c1f
coalg <algebroid0@gmail.com>
2025-01-20 01:29:19 +0900

    small refactoring

--- aoc/2024/02/day02_01.hs
+++ aoc/2024/02/day02_01.hs
@@ -14,14 +14,18 @@ g (x, y) = y - x
 monotone :: [Int] -> Bool
 monotone xs = all (< 0) xs || all (> 0) xs
 
+smallGap :: [Int] -> Bool
 smallGap = all (abs >>> ((>= 1) &&& (<= 3)) >>> uncurry (&&))
 
+isSafe :: [Int] -> Bool
 isSafe xs =  monotone xs && smallGap xs
+
+solution1 :: String -> Int
+solution1 = length . filter id . map (isSafe . map g . pairs . f . words) . lines
 
 main = do
     contents <- readFile "input.txt"
-    print
-        . length . filter id
-        . map (isSafe . map g . pairs . f . words) $ lines contents
+    print . solution1 $ contents
+
 f :: [String] -> [Int]
 f = map read
