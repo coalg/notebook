efe25662cd9776b8a64dc492c1f1cee5a5dc732a
coalg <algebroid0@gmail.com>
2025-01-21 00:22:05 +0900

    add quiz mode

--- languages/rust/rust-for-backend-engineer/ch03/arithmetic-operations-quiz/src/main.rs
+++ languages/rust/rust-for-backend-engineer/ch03/arithmetic-operations-quiz/src/main.rs
@@ -4,41 +4,45 @@ fn main() {
     let mut num_of_correct = 0;
 
     while num_of_correct < 3 {
-        let op1 = rand::thread_rng().gen_range(0..100);
-        let op2 = rand::thread_rng().gen_range(0..100);
-        println!("{} + {} = ??", op1, op2);
-        println!("?? の値を入力してください：");
-        let mut ans_input = String::new();
-        std::io::stdin().read_line(&mut ans_input).unwrap();
+        let quiz_mode = rand::thread_rng().gen_range(1..=2);
 
-        let ans_input = ans_input.trim().parse::<i32>().unwrap();
-        dbg!(ans_input);
-        if dbg!(ans_input == op1 + op2) {
-            println!("正解！");
-            num_of_correct += 1;
-            if num_of_correct >= 3 {
-                break;
+        match quiz_mode {
+            1 => {
+                let op1 = rand::thread_rng().gen_range(0..100);
+                let op2 = rand::thread_rng().gen_range(0..100);
+                println!("{} + {} = ??", op1, op2);
+                println!("?? の値を入力してください：");
+                let mut ans_input = String::new();
+                std::io::stdin().read_line(&mut ans_input).unwrap();
+
+                let ans_input = ans_input.trim().parse::<i32>().unwrap();
+                dbg!(ans_input);
+                if dbg!(ans_input == op1 + op2) {
+                    println!("正解！");
+                    num_of_correct += 1;
+                } else {
+                    println!("不正解！")
+                }
             }
-        } else {
-            println!("不正解！")
+            2 => {
+                let op1 = rand::thread_rng().gen_range(0..100);
+                let op2 = rand::thread_rng().gen_range(0..100);
+                println!("{} - {} = ??", op1, op2);
+                println!("?? の値を入力してください：");
+                let mut ans_input = String::new();
+                std::io::stdin().read_line(&mut ans_input).unwrap();
+
+                let ans_input = ans_input.trim().parse::<i32>().unwrap();
+                dbg!(ans_input);
+                if dbg!(ans_input == op1 - op2) {
+                    println!("正解！");
+                    num_of_correct += 1;
+                } else {
+                    println!("不正解！")
+                }
+            }
+            _ => unreachable!(),
         }
-
-        let op1 = rand::thread_rng().gen_range(0..100);
-        let op2 = rand::thread_rng().gen_range(0..100);
-        println!("{} - {} = ??", op1, op2);
-        println!("?? の値を入力してください：");
-        let mut ans_input = String::new();
-        std::io::stdin().read_line(&mut ans_input).unwrap();
-
-        let ans_input = ans_input.trim().parse::<i32>().unwrap();
-        dbg!(ans_input);
-        if dbg!(ans_input == op1 - op2) {
-            println!("正解！");
-            num_of_correct += 1;
-        } else {
-            println!("不正解！")
-        }
-
         println!("i32 が扱えるデータ範囲: {} ~ {}", i32::MIN, i32::MAX);
         println!("u32 が扱えるデータ範囲: {} ~ {}", u32::MIN, u32::MAX);
     }
