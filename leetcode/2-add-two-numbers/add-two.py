eadfe94b098eaa6b72075d500fdc592c8c2b66e8
coalg <algebroid0@gmail.com>
2025-03-16 20:46:03 +0900

    add two

---
+++ leetcode/2-add-two-numbers/add-two.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        node = dummy
+
+        total = carry = 0
+
+        while l1 or l2 or carry:
+            total = carry
+
+            if l1:
+                total += l1.val
+                l1 = l1.next
+
+            if l2:
+                total += l2.val
+                l2 = l2.next
+
+            node.next = ListNode(total % 10)
+            carry = total // 10
+
+            node = node.next
+        return dummy.next
