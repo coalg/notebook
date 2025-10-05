40893135712c44a0ea89b8ebcf7f12764e66f7a5
coalg <algebroid0@gmail.com>
2025-10-05 23:41:31 +0900

    add solution

--- atcoder/abc/0418/c.py
+++ atcoder/abc/0418/c.py
@@ -1,8 +1,20 @@
+import bisect
+
 N, Q = map(int, input().split())
-xs = list(map(int, input().split()))
-s = sum(xs)
+xs = sorted(map(int, input().split()))
+l = len(xs)
 
-bs = []
+sums = [0] * (N + 1)
+
+for i in range(l):
+    sums[i+1] += sums[i] + xs[i]
+
 for _ in range(Q):
-    bs.append(int(input()))
+    b = int(input())
+    i = bisect.bisect_left(xs, b)
 
+    ans = sums[i] + (b - 1) * (l - i) + 1
+    if ans > sums[-1]:
+        print(-1)
+    else:
+        print(ans)
